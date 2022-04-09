5f9c8599d28d0d8c9e455112ba61fd5f8c4d7a4f
Piotr Bilski <bilski.piotr@outlook.com>
2022-04-09 10:33:09 +0200

    Dodaj pliki projektów.

---
+++ PO-W-507/Program.cs
@@ -0,0 +1,80 @@
+﻿// Autor. mgr. inż Piotr Bilski
+// Programowanie Obiektowe
+
+using System;
+
+namespace PO_W_507
+{
+    // ======== 1 ========
+    class Figura
+    {
+        public string FigType { get; set; }
+        public void UstawNaroznik()
+        {
+            Console.WriteLine("  Ustawiono punkt zaczepienia");
+        }
+    }
+    class Rownoleglobok : Figura
+    {
+        public void Narysuj()
+        {
+            Console.WriteLine("  Narysowano równoległobok");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // utworzenie obiektu klasy pochodnej
+            Rownoleglobok obj1 = new Rownoleglobok();
+            // wywołanie właściwości zadeklarowanej w klasie
+            // nadrzędnej i ustawienie typu opisowego
+            obj1.FigType = "Prostokąt";
+            // wyświetlenie wartości pola prywatnego FigType
+            Console.WriteLine("\n  " + obj1.FigType);
+            // wywołanie metody z nadklasy
+            obj1.UstawNaroznik();
+            // wywołanie metody z klasy własnej
+            obj1.Narysuj();
+            Console.ReadKey();
+        }
+    }
+
+    // ========  2  ========
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        // tworzenie obiektu typu pochodnego
+    //        Rownoleglobok obj1 = new Rownoleglobok();
+    //        obj1.FigType = "Prostokąt";
+    //        Console.WriteLine(obj1.FigType);
+    //        obj1.UstawNaroznik();
+    //        obj1.Narysuj();
+
+    //        // tworzenie obiektu typu nadrzędnego
+    //        Figura obj2 = new Figura();
+    //        obj2.FigType = "JakaśInnaFigura";
+    //        Console.WriteLine(obj2.FigType);
+    //        obj2.UstawNaroznik();
+    //        // wywołanie metody z typu pochodnego
+    //        obj2.Narysuj();
+    //    }
+    //}
+
+    // ======== 3 ========
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Rownoleglobok obj1 = new Rownoleglobok();
+    //        // przepisanie obiekty klasy pochodnej do 
+    //        // obiektu klasy nadrzędnej
+    //        Figura obj2 = obj1;
+    //        Console.WriteLine(obj2.FigType);
+    //        obj2.UstawNaroznik();
+    //        obj2.Narysuj();
+    //        Console.ReadKey();
+    //    }
+    //}
+}
